680b2748c838ca12304966e493a0d926491c74d3
“haoyong” <a1148435545@163.com>
2021-11-29 18:14:54 +0800

    entity

---
+++ src/main/java/com/entity/AliRowsEntity.java
@@ -0,0 +1,32 @@
+package com.entity;
+
+
+/**
+ * <p>
+ * TODO 描述
+ * </p>
+ *
+ * @author hyong
+ * @since 2021/11/29
+ */
+
+public class AliRowsEntity {
+	private String word;
+	private Integer rowId;
+
+	public String getWord() {
+		return word;
+	}
+
+	public void setWord(String word) {
+		this.word = word;
+	}
+
+	public Integer getRowId() {
+		return rowId;
+	}
+
+	public void setRowId(Integer rowId) {
+		this.rowId = rowId;
+	}
+}
